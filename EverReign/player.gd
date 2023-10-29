4d7f45d2152c3fd30da417642d353454721340d9
Brighton Gold <113638673+Brighton-Gold@users.noreply.github.com>
2023-10-28 18:08:02 -0600

    Add files via upload
    Has player movement and town. Need to include a camera node.

---
+++ EverReign/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+const SPEED = 70.0
+const JUMP_VELOCITY = -400.0
+
+var input_direction: get = _get_input_direction
+var sprite_direction = "Down": get =_get_sprite_direction
+@onready var sprite = $AnimatedSprite2D
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	velocity = input_direction * SPEED
+	move_and_slide()
+	
+	set_animation("Walk")
+	if velocity == Vector2.ZERO:
+		sprite.stop()
+		
+func set_animation(animation):
+	var direction = "Side" if sprite_direction in ["Left", "Right"] else sprite_direction
+	sprite.play(animation + direction)
+	sprite.flip_h = (sprite_direction == "Left")
+	
+func _get_input_direction():
+	var x = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
+	var y = -int(Input.is_action_pressed("ui_up")) + int(Input.is_action_pressed("ui_down"))
+	input_direction = Vector2(x,y).normalized()
+	return input_direction
+
+func _get_sprite_direction():
+	match input_direction:
+		Vector2.LEFT:
+			sprite_direction = "Left"
+		Vector2.RIGHT:
+			sprite_direction = "Right"
+		Vector2.UP:
+			sprite_direction = "Up"
+		Vector2.DOWN:
+			sprite_direction = "Down"
+	return sprite_direction
+	
+
